83d2e60236efbbe62acaa2d61ea0f8a6c8a0eb73
Alexandra Ramlogan <90638917+AlexRamlogan02@users.noreply.github.com>
2024-01-24 17:31:17 -0500

    Fonts

---
+++ var/www/html/js/login.js
@@ -0,0 +1,6 @@
+function login(){
+    const usernameInput = document.getElementById("username").value;
+    const passwordInput = document.getElementById("password".value)
+
+    print(usernameInput + passwordInput)
+}
