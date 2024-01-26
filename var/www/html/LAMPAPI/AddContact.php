9bce26f6debf7816914def69ac600c7619f5e730
sophia <147104540+sophiagn@users.noreply.github.com>
2024-01-25 20:10:19 -0500

    Update AddContact.php

--- var/www/html/LAMPAPI/AddContact.php
+++ var/www/html/LAMPAPI/AddContact.php
@@ -1,22 +1,23 @@
 
 <?php
 
-	$inData = getRequestInfo();
+    $inData = getRequestInfo();
 	
     $name = $inData["name"];
-	$phone = $inData["phone"];
-	$email = $inData["email"];
+    $phone = $inData["phone"];
+    $email = $inData["email"];
     $userId = $inData["userId"];
 
-    // connect to Users
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    // connect to database
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	
     // connection error
-    if ($conn->connect_error) {
+    if ($conn->connect_error)
 		returnWithError( $conn->connect_error );
 	
     // add contact
-    } else {
+    else 
+	{
 		$stmt = $conn->prepare("INSERT into Contacts (Name, Phone, Email, UserID) VALUES(?,?,?,?)");
 		$stmt->bind_param("sisi", $name, $phone, $email, $userId);
 		$stmt->execute();
