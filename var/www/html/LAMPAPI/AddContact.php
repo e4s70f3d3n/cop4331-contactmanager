05b396c5592d1f4e6f792fc095523c68930140a8
sophia <147104540+sophiagn@users.noreply.github.com>
2024-01-25 19:03:50 -0500

    Add files via upload

---
+++ var/www/html/LAMPAPI/AddContact.php
@@ -0,0 +1,43 @@
+<?php
+	$inData = getRequestInfo();
+	
+    $name = $inData["name"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+
+    // connect to Users
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	
+    // connection error
+    if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (Name, Phone, Email) VALUES(?,?,?)");
+		$stmt->bind_param("sis", $name, $phone, $email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
