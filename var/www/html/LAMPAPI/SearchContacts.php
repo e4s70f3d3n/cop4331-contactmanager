4c3fbe5d37c1e2cccb7905cf024d9a0bb84a4779
sophia <147104540+sophiagn@users.noreply.github.com>
2024-01-30 20:32:14 -0500

    Update SearchContacts.php

--- var/www/html/LAMPAPI/SearchContacts.php
+++ var/www/html/LAMPAPI/SearchContacts.php
@@ -3,28 +3,41 @@
 
 	$inData = getRequestInfo();
 	
-    $firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-	$phone = $inData["phone"];
-	$email = $inData["email"];
-    $ID = $inData["ID"];
+	$searchResults = "";
+	$searchCount = 0;
 
     // connect to database
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	
+
     // connection error
-    if ($conn->connect_error)
+	if ($conn->connect_error) 
 		returnWithError( $conn->connect_error );
-	
-    // update contact
-    else 
+	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE ID=?");
-		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $ID);
+		// find search results based on input parameter
+		$stmt = $conn->prepare("select * from Contacts where (FirstName like ? or LastName like ? or Phone like ? or Email like ?) and UserID=?");
+		$searchParameter = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ssssi", $searchParameter, $searchParameter, $searchParameter, $searchParameter, $inData["userId"]);
 		$stmt->execute();
+		$result = $stmt->get_result();
+		
+		// add each result to a single string
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+				$searchResults .= ",";
+			
+			$searchResults .= '{"id":"' . $row["ID"] . '","firstName":"' . $row["FirstName"] . '","lastName":"' . $row["LastName"] . '","phone":"' . $row["Phone"] . '","email":"' . $row["Email"] . '"}';
+			$searchCount++;
+		}
+		
+		if( $searchCount == 0 )
+			returnWithError( "No Records Found" );
+		else
+			returnWithInfo( $searchResults );
+		
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
 	function getRequestInfo()
@@ -41,6 +54,12 @@
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
