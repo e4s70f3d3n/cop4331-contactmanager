f6704187a825598f531049a27e20294de477eab8
Alexandra Ramlogan <90638917+AlexRamlogan02@users.noreply.github.com>
2024-01-30 19:06:57 -0500

    Update script.js

--- var/www/html/js/script.js
+++ var/www/html/js/script.js
@@ -74,71 +74,33 @@ function doRegister() {
 	const xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-Type", "application/json; charset = UTF-8");
-
+	
 	try {
 		xhr.onreadystatechange = function () {
 
-			if (this.status == 409) {
-				window.location.href = "landing.html";
-                document.getElementById("registerResult").innerHTML = "Username already exists";
-                return;
-            }
 
             if (this.readyState == 4 && this.status == 200) {
 
                 let jsonObject = JSON.parse(xhr.responseText);
                 userId = jsonObject.id;
-                //document.getElementById("registerResult").innerHTML = "User created";
+                document.getElementById("registerResult").innerHTML = "Complete";
                 firstName = jsonObject.firstName;
                 lastName = jsonObject.lastName;
-				
                 saveCookie();
                 window.location.href = "landing.html";
             }
-        };
 
+			else{
+				//window.location.href = "landing.html";
+                document.getElementById("registerResult").innerHTML = `Error ${xhr.status}: ${xhr.responseText}`;;
+                return false;
+            }
+        };
 		xhr.send(body);
 	} catch (err) {
 		document.getElementById("loginResult").innerHTML = err.message;
+		return;
 	}
-	/*try
-	{
-		xhr.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				let jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;	
-				document.getElementById("registerResult").innerHTML = "User created";
-					return;
-			}
-		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
-
-				saveCookie();
-	
-				window.location.href = "color.html";
-			};
-	}
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("registerResult").innerHTML = err.message;
-	}
-	/*xhr.onreadystatechange = function(){
-
-		if(xhr.readyState == 4 && xhr.status == 200){
-			//document.getElementById("registerResult").innerHTML = xhr.responseText;
-			//saveCookie();
-			//window.open("http://cop4331-15.xyz/");
-			//window.close();
-		}
-		else{
-			document.getElementById("registerResult").innerHTML = `Error ${xhr.status}: ${xhr.responseText}`;
-		}
-	}
-	xhr.send(body);*/
 }
 
 function saveCookie() {
