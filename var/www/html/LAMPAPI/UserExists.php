b88fd1ba0c0d177503c320525ddc5720c34d55ef
sophia <147104540+sophiagn@users.noreply.github.com>
2024-01-31 19:08:15 -0500

    Add files via upload

---
+++ var/www/html/LAMPAPI/UserExists.php
@@ -0,0 +1,47 @@
+
+<?php
+
+	$inData = getRequestInfo();
+
+	// connect to database
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	
+	// connection error
+	if( $conn->connect_error ) 
+		returnWithError( $conn->connect_error );
+
+    // check that the user exists
+	else 
+	{
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $inData["login"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if ( $row = $result->fetch_assoc() )
+            returnWithError("");
+		else
+			returnWithError("No Records Found");
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
