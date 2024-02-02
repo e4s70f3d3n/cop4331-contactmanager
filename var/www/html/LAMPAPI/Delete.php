ce6af73943725440b9241620038108a7d63dd188
sophia <147104540+sophiagn@users.noreply.github.com>
2024-02-01 19:30:29 -0500

    Update Delete.php

--- var/www/html/LAMPAPI/Delete.php
+++ var/www/html/LAMPAPI/Delete.php
@@ -1,56 +1,42 @@
+
 <?php
 
-include 'config.php';
+	$inData = getRequestInfo();
+    $ID = $inData["ID"];
 
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Mehtods: DELETE");
-header("Access-Control-Allow-Headers:Access-Control-Allow-Mehtods,Content-Type,Access-Control-Allow-Mehtods,Authorization,X-Requested-With");
+    // connect to database
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	
+    // connection error
+    if ($conn->connect_error)
+		returnWithError( $conn->connect_error );
+	
+    // delete contact
+    else 
+	{
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=?");
+		$stmt->bind_param("i", $ID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
 
-$requestMethod = $_SERVER["REQUEST_METHOD"];
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-if($requestMethod == "DELETE") {
-    $deleteContact = deleteContact($_GET);
-    echo $deleteContact;
-} else {
-    $data = [
-        'message' => $requestMethod, 'Method Not Allowed',
-        'status' => 405,
-    ];
-    header("HTTP/1.0 405 Method Not Allowed");
-    echo json_encode($data);
-}
-
-function deleteContact($inData)
-{
-    global $conn;
-
-    if (!isset($inData['id'])) {
-        return error422('Error: Contact ID not found in URL.');
-    } elseif ($inData['id'] == null) {
-        return error422('Error: Contact ID is NULL. Please enter the contact ID.');
-    }
-
-    $contactID = mysqli_real_escape_string($conn, $inData['ID']);
-
-    $search = "DELETE FROM Contacts WHERE id='$contactID' LIMIT 1";
-    $search_results = mysqli_query($conn, $search);
-
-    if ($search_results) {
-        $data = [
-            'message' => 'Contact Deleted Successfully.',
-            'status' => 200,
-        ];
-        header("HTTP/1.0 200 OK");
-        return json_encode($data);
-    } else {
-        $data = [
-            'message' => 'Contact Not Found',
-            'status' => 404,
-        ];
-        header("HTTP/1.0 404 Not Found");
-        return json_encode($data);
-    }
-}
-
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
 ?>
